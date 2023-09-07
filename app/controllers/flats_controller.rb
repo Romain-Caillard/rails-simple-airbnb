760a8b4d8e3b9cb37678694b1d4e05cfcec67b53
romain caillard <mttdiamond@gmail.com>
2023-09-07 16:03:48 +0200

    step simple_airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,31 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :destroy]
+
   def index
     @flats = Flat.all
   end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params[:flat])
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def
 end
